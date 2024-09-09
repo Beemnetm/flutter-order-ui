fd92b1f7a92020785117a1e6da672883a3fb09a9
Beemnetm <beemnetmulugeta@gmail.com>
2024-09-09 15:16:41 +0300

    SVG

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class homepage extends StatelessWidget {
   const homepage({super.key});
@@ -17,8 +18,15 @@ class homepage extends StatelessWidget {
         elevation: 0.0,
         leading: Container(
           margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/Arrow.svg',
+            height: 20,
+            width: 20,
+          ),
           decoration: BoxDecoration(
-              color: Colors.black, borderRadius: BorderRadius.circular(10)),
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
         ),
       ),
     );
